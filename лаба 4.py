dfc0ba4a5248a007b85a6f573aecee97756a7be3
MokrushinIvan <dimkaaaaa2018@mail.ru>
2023-03-30 20:25:07 +0400

    laba

--- лаба 4.py
+++ лаба 4.py
@@ -20,6 +20,37 @@ def b():
 b()
 
 def c():
-a = [1,2,3,4,5,5,6,6,7]
-print(*filter(lambda x : a.count(x) > 1, a))
+
+    a = [1,2,3,4,5,5,6,6,7]
+    print(*filter(lambda x : a.count(x) > 1, a))
+
 c()
+
+def g():
+
+    user1 = ['Иванов', 'Смирнов', 'Кузнецов', 'Попов', 'Васильев', 'Петров', 'Соколов', 'Михайлов', 'Новиков', 'Федоров']
+    user2 = ['Морозов', 'Волков', 'Лебедев', 'Иванов', 'Егоров', 'Павлов', 'Козлов', 'Иванов', 'Николаев']
+    print(user1)
+    print(user2)
+    user1.sort()
+    user2.reverse()
+    users1 = user1.copy()
+    users2 = user2.copy()
+    del user1[5:11:1]
+    del users1[0:5:1]
+    del user2[5:11:1]
+    del users2[0:5:1]
+    sport1 = user1 + users1
+    sport2 = user2 + users2
+    sport2.sort()
+    sp1 = tuple(sport1)
+    sp2 = tuple(sport2)
+    p = 'Иванов'
+    n = 0
+    for i in sp2:
+        if i == p:
+            n = n + 1
+    print('команда: ', sp2, 'кол-во участников: ', len(sp2), ' Количество участников с фамилией Иванов ', n)
+
+
+g()
